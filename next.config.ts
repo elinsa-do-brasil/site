b422072e89a3e2dc814f18a7874415a7d5974711
Raave Aires <developer@raave.me>
2026-05-06 22:23:26 -0300

    Prepara base visual e dependências de UI

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,23 @@
-import createMDX from '@next/mdx'
-import { withPayload } from '@payloadcms/next/withPayload'
+import createMDX from "@next/mdx";
+import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+    ],
+  },
   reactCompiler: true,
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
 const withMDX = createMDX({
   extension: /\.(md|mdx)$/,
-})
+});
 
 export default withPayload(withMDX(nextConfig));
