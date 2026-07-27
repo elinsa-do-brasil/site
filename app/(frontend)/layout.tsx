d48b376eb10ec3ba544c676df3d51ed52aaeeaf6
Raave Aires <developer@raave.me>
2026-07-27 13:10:50 +0000

    feat: adiciona suporte ao Vercel Analytics e atualiza dependências

--- app/(frontend)/layout.tsx
+++ app/(frontend)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "../globals.css";
+import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Footer } from "@/components/footer";
 import { FrontendShell } from "@/components/frontend-shell";
@@ -88,6 +89,7 @@ export default function RootLayout({
           </TooltipProvider>
         </ThemeProvider>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
